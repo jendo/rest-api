390dda013dcb8b5cc1a2143d2d06dcc6da36de73
Michal Jenis <jenis.michal@gmail.com>
2018-02-13 17:27:04 +0100

    Basic skelete with nette di

---
+++ src/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+use Nette\DI\Compiler;
+use Nette\DI\ContainerLoader;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$baseDir = dirname(__DIR__);
+require_once $baseDir . '/vendor/autoload.php';
+
+$loader = new ContainerLoader($baseDir . '/temp', $autoRebuild = true);
+$class = $loader->load(
+    function (Compiler $compiler) use ($baseDir) {
+        $compiler->addConfig(['parameters' => ['baseDir' => $baseDir]]);
+        $compiler->loadConfig($baseDir . '/src/config/config.neon');
+    }
+);
+
+return new $class;
---
+++ www/index.php
@@ -0,0 +1,10 @@
+<?php
+use App\Application;
+use Nette\DI\Container;
+
+/** @var Container $container */
+$container = require __DIR__ . '/../src/bootstrap.php';
+
+/** @var Application $application */
+$application = $container->getService('application');
+$application->run();
